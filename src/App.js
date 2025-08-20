107093789c97a37043a316238d611e261a49fd4b
rlanderbryan-ship-it <rlanderbryan@gmail.com>
2025-08-20 15:19:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sample
+          sample project
         
         </p>
         <a
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React js
         </a>
       </header>
     </div>
